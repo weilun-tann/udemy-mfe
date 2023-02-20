b13c15b06359016d856fdbaefaaaad53340d76a6
weilun-tann <weilun.tann@gmail.com>
2023-02-20 15:50:40 +0800

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
-    <>
-      <h1>This is the Container</h1>
-      <MarketingApp />
-    </>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
